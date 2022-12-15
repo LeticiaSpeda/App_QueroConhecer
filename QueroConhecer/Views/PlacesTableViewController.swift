2abf99ee15d047d136057099f334815ee54c64d9
Leticia Speda <leticiaspeda@gmail.com>
2022-12-15 11:37:58 -0300

    Feature(PlacesTableViewController): criado texto e opcao de deletar o local salvo: Description: - Texto na navigation bar para futuramente o usuario optar por visualizar todos os locais - Adicionado a celula opcao de deletar da lista o local salvo - Texto na tela inicial para o usuario pesquisar algum local caso nunca tenha salvado algum local ou caso tenha excluido todos locais salvos

--- QueroConhecer/Views/PlacesTableViewController.swift
+++ QueroConhecer/Views/PlacesTableViewController.swift
@@ -11,6 +11,15 @@ final class PlacesTableViewController: UITableViewController {
     var places: [Place] = []
     let ud = UserDefaults.standard
     
+    
+    private lazy var lbNoPlaces: UILabel = {
+        let label = UILabel()
+        label.text = "Cadastre os locais que deseja conhecer\nclicando no botão + acima"
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureStyle()
@@ -20,6 +29,9 @@ final class PlacesTableViewController: UITableViewController {
             forCellReuseIdentifier: PlacesTableViewCell.identifier
             
         )
+        
+        
+        
     }
     
     private func loadPlaces() {
@@ -39,7 +51,7 @@ final class PlacesTableViewController: UITableViewController {
         let json = try? JSONEncoder().encode(places)
         ud.set(json, forKey: "places")
     }
-
+    
     private func configureStyle() {
         view.backgroundColor = .white.withAlphaComponent(0.9)
         navigationItem.title = "Quero Conhecer"
@@ -47,18 +59,18 @@ final class PlacesTableViewController: UITableViewController {
         let appearance = UINavigationBarAppearance()
         appearance.backgroundColor = .white.withAlphaComponent(0.5)
         appearance.titleTextAttributes = [.foregroundColor: UIColor.black]
-
+        
         navigationController?.navigationBar.standardAppearance = appearance
         navigationController?.navigationBar.compactAppearance = appearance
         navigationController?.navigationBar.scrollEdgeAppearance = appearance
-
+        
         navigationItem.rightBarButtonItem = .init(
             image: .init(systemName: "plus"),
             style: .plain, target: self,
             action: #selector(addRoute)
         )
     }
-
+    
     @objc func addRoute() {
         let controller = PlaceFinderViewController()
         controller.delegate = self
@@ -66,23 +78,33 @@ final class PlacesTableViewController: UITableViewController {
         controller.modalTransitionStyle = .crossDissolve
         self.present(controller, animated: true)
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-
+        if places.count > 0 {
+            let btShowAll = UIBarButtonItem(title: "Mostrar todos no mapa", style: .plain, target:  self, action: #selector(showAll))
+            navigationItem.leftBarButtonItem = btShowAll
+            tableView.backgroundView = nil
+        } else {
+            navigationItem.leftBarButtonItem = nil
+            tableView.backgroundView = lbNoPlaces
+        }
         return places.count
     }
-
+    
+    @objc func showAll() {
+        
+    }
     override func tableView(_ tableView: UITableView,cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
+        
         if let cell = tableView.dequeueReusableCell(withIdentifier: PlacesTableViewCell.identifier,
-            for: indexPath) as? PlacesTableViewCell {
+                                                    for: indexPath) as? PlacesTableViewCell {
             let place = places[indexPath.row]
             cell.titleLabel.text = place.name
             return cell
         }
         return UITableViewCell()
     }
-
+    
     override func tableView(
         _ tableView: UITableView,
         didSelectRowAt indexPath: IndexPath
@@ -92,6 +114,15 @@ final class PlacesTableViewController: UITableViewController {
         navigation.modalPresentationStyle = .overFullScreen
         present(navigation, animated: true)
     }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            places.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            savePlaces()
+        }
+    }
+    
 }
 
 extension PlacesTableViewController: PlaceFinderDelegate {
